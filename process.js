7420219cc1121e253a2f83e90cf749bfb24d26d0
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2024-08-20 20:51:47 +0700

    Update process.js

--- process.js
+++ process.js
@@ -1,1 +1,11 @@
-alert("Tải extention thành công");
+const urlApp = window.location.href;
+debugger;
+if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
+    var btnExcelImport = document.createElement("button");
+    btnExcelImport.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
+    btnExcelImport.className = "btn btn-sm btn-primary";
+    btnExcelImport.id = "btnKiemTraTheBHYT";
+    btnExcelImport.type = "button";
+    btnExcelImport.click = "myFunction";
+    document.getElementById("baohiem5nam_label").parentElement.appendChild(btnExcelImport);
+}
