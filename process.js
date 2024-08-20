812513e4b9f7b8613fdaaaaf7af176e0b4f51993
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2024-08-20 21:07:41 +0700

    Update process.js

--- process.js
+++ process.js
@@ -1,11 +1,15 @@
 const urlApp = window.location.href;
-debugger;
-if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
-    var btnExcelImport = document.createElement("button");
-    btnExcelImport.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
-    btnExcelImport.className = "btn btn-sm btn-primary";
-    btnExcelImport.id = "btnKiemTraTheBHYT";
-    btnExcelImport.type = "button";
-    btnExcelImport.click = "myFunction";
-    document.getElementById("baohiem5nam_label").parentElement.appendChild(btnExcelImport);
-}
+
+window.addEventListener('load', () => {
+    alert("Tải xong Dom");
+});
+//debugger;
+//if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
+//    var btnExcelImport = document.createElement("button");
+//    btnExcelImport.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
+//    btnExcelImport.className = "btn btn-sm btn-primary";
+//    btnExcelImport.id = "btnKiemTraTheBHYT";
+//    btnExcelImport.type = "button";
+//    btnExcelImport.click = "myFunction";
+//    document.getElementById("baohiem5nam_label").parentElement.appendChild(btnExcelImport);
+//}
