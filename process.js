0805ed6f0945e766c8d2b3183772102c499eccd5
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2024-08-21 20:38:21 +0700

    Update process.js

--- process.js
+++ process.js
@@ -2,6 +2,24 @@ const urlApp = window.location.href;
 
 window.addEventListener('load', () => {
     if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
+        debugger;
+        fetch('Api_address')
+        .then(response => {
+          if (response.ok) {
+            return response.json(); // Parse the response data as JSON
+          } else {
+            throw new Error('API request failed');
+          }
+        })
+        .then(data => {
+          // Process the response data here
+          console.log(data); // Example: Logging the data to the console
+        })
+        .catch(error => {
+          // Handle any errors here
+          console.error(error); // Example: Logging the error to the console
+        });
+
         var btnKiemTraThongTin = document.createElement('button');
         btnKiemTraThongTin.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
         btnKiemTraThongTin.className = "btn btn-sm btn-primary";
