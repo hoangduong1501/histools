840bb6d86f11acb52a3fa0d5e4872158ad62d1a3
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2024-08-21 21:41:22 +0700

    Update process.js

--- process.js
+++ process.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
       .then(data => {
         debugger;
         var nhanVien = data.find((item) => item.MA_NHANVIEN_HIS.toString() === sessionStorage.getItem("userId"));
-        localStorage.setItem("authExtention", nhanVien.SO_CCCD_NV + '|' + nhanVien + TEN_NHANVIEN);
+        localStorage.setItem("authExtention", nhanVien.SO_CCCD_NV + '|' + nhanVien.TEN_NHANVIEN);
       })
       .catch(error => {
         // Handle any errors here
