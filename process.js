2feb10ea37436aee4a7b9fe3bcaada6b5e7e40e5
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2024-08-21 20:58:17 +0700

    Update process.js

--- process.js
+++ process.js
@@ -1,37 +1,41 @@
 const urlApp = window.location.href;
 
 window.addEventListener('load', () => {
-    if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
-        debugger;
-        fetch(window.location.origin + '/web_his/danhsach_canbo_nhanvien_sudungapiBHXH')
-        .then(response => {
+  if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
+    const maNhanVienHis = sessionStorage.getItem("userId");
+
+    fetch(window.location.origin + '/web_his/danhsach_canbo_nhanvien_sudungapiBHXH')
+      .then(response => {
+        if (response.ok) {
           debugger;
-          if (response.ok) {
-            return response.json(); // Parse the response data as JSON
-          } else {
-            throw new Error('API request failed');
-          }
-        })
-        .then(data => {
-          // Process the response data here
-          console.log(data); // Example: Logging the data to the console
-        })
-        .catch(error => {
-          // Handle any errors here
-          console.error(error); // Example: Logging the error to the console
-        });
+          var lstNhanVien = response.json(); // Parse the response data as JSON
+          return;
+        } else {
+          throw new Error('API request failed');
+        }
+      })
+      .then(data => {
+        // Process the response data here
+        console.log(data); // Example: Logging the data to the console
+      })
+      .catch(error => {
+        // Handle any errors here
+        console.error(error); // Example: Logging the error to the console
+      });
 
-        var btnKiemTraThongTin = document.createElement('button');
-        btnKiemTraThongTin.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
-        btnKiemTraThongTin.className = "btn btn-sm btn-primary";
-        btnKiemTraThongTin.id = "btnKiemTraThongTin";
-        btnKiemTraThongTin.type = "button";
-        btnKiemTraThongTin.click = "myFunction";
-        document.getElementById("baohiem5nam_label").parentElement.appendChild(btnKiemTraThongTin);
-    }
+      
 
-    document.getElementById("btnKiemTraThongTin").addEventListener("click", (e) => {
-        alert("Đã bật kiểm tra thẻ BHYT");
-    });
+    var btnKiemTraThongTin = document.createElement('button');
+    btnKiemTraThongTin.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
+    btnKiemTraThongTin.className = "btn btn-sm btn-primary";
+    btnKiemTraThongTin.id = "btnKiemTraThongTin";
+    btnKiemTraThongTin.type = "button";
+    btnKiemTraThongTin.click = "myFunction";
+    document.getElementById("baohiem5nam_label").parentElement.appendChild(btnKiemTraThongTin);
+  }
+
+  document.getElementById("btnKiemTraThongTin").addEventListener("click", (e) => {
+    alert("Đã bật kiểm tra thẻ BHYT");
+  });
 });
 
