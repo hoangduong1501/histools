54868796f2756bae120e1aa412830d0911abcd5b
hoangduong1501 <67629267+hoangduong1501@users.noreply.github.com>
2024-09-02 08:49:29 +0700

    Update process.js

--- process.js
+++ process.js
@@ -2,31 +2,57 @@ const urlApp = window.location.href;
 
 window.addEventListener('load', async () => {
   if (urlApp.includes("tiepnhanbenhnhan") || urlApp.includes("tiepnhanBANT") || urlApp.includes("tiepnhannhapvien")) {
-    if (localStorage.getItem("authExtention") === null || fromBinary(localStorage.getItem("authExtention")).split('|')[0] !== sessionStorage.getItem("userId")) {
-      localStorage.removeItem("authExtention");
+    var thongTinNhanVien = await fetch(window.location.origin + '/web_his/danhsach_canbo_nhanvien_sudungapiBHXH')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('API request failed');
+        }
+      })
+      .then(data => {
+        var nhanVien = data.find((item) => item.MA_NHANVIEN_HIS.toString() === sessionStorage.getItem("userId"));
+        if (nhanVien === undefined) {
+          alert('Tài khoản này không có quyền tra cứu');
+          return false;
+        } else {
+          localStorage.setItem("authExtention", toBinary(nhanVien.SO_CCCD_NV + '|' + nhanVien.TEN_NHANVIEN));
+          return true;
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-      var quyenTraCuu = await layThongTinNguoiTraCuu();
-      if (quyenTraCuu) {
-        debugger
-      } else {
-        debugger;
+      if (thongTinNhanVien) {
+        alert("Nhân viên có quyền tra cứu");
+      }else{
+        alert("Nhân viên không có quyền tra cứu");
       }
 
 
+    // if (localStorage.getItem("authExtention") === null || fromBinary(localStorage.getItem("authExtention")).split('|')[0] !== sessionStorage.getItem("userId")) {
+    //   localStorage.removeItem("authExtention");
 
+    //   var quyenTraCuu = await layThongTinNguoiTraCuu();
+    //   if (quyenTraCuu) {
+    //     debugger
+    //   } else {
+    //     debugger;
+    //   }
 
-      var btnKiemTraThongTin = document.createElement('button');
-      btnKiemTraThongTin.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
-      btnKiemTraThongTin.className = "btn btn-sm btn-primary";
-      btnKiemTraThongTin.id = "btnKiemTraThongTin";
-      btnKiemTraThongTin.type = "button";
-      btnKiemTraThongTin.click = "myFunction";
-      document.getElementById("baohiem5nam_label").parentElement.appendChild(btnKiemTraThongTin);
-    }
+    //   var btnKiemTraThongTin = document.createElement('button');
+    //   btnKiemTraThongTin.innerHTML = '<i class="fa fa-check-square-o" aria-hidden="true"></i> Kiểm tra thẻ BHYT';
+    //   btnKiemTraThongTin.className = "btn btn-sm btn-primary";
+    //   btnKiemTraThongTin.id = "btnKiemTraThongTin";
+    //   btnKiemTraThongTin.type = "button";
+    //   btnKiemTraThongTin.click = "myFunction";
+    //   document.getElementById("baohiem5nam_label").parentElement.appendChild(btnKiemTraThongTin);
+    // }
 
-    document.getElementById("btnKiemTraThongTin").addEventListener("click", (e) => {
-      alert("Đã bật kiểm tra thẻ BHYT");
-    });
+    // document.getElementById("btnKiemTraThongTin").addEventListener("click", (e) => {
+    //   alert("Đã bật kiểm tra thẻ BHYT");
+    // });
   }
 });
 
